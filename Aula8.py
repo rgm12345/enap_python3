5eaf217764c54a8425d6ec6842870f4afba77a1f
rgm12345 <rebeca.guerreiromachado@gmail.com>
2024-11-13 10:29:28 -0400

    Update Aula8.py

--- Aula8.py
+++ Aula8.py
@@ -10,3 +10,9 @@ st.markdown("Este é texto")
 st.caption("Esta é a a legenda")
 st.code("x=2021")
 st.latex(r''' a+a r^1+a r^2+a r^3 ''')
+
+grau_satisfacao = st.select_slider(
+    'Qual o seu grau de satisfação?',
+    options=range(0, 101))
+
+st.write('O cliente respondeu:', grau_satisfacao)
