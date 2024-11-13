e0c840931e76d156a137170a2754ad61f5e3a1df
rgm12345 <rebeca.guerreiromachado@gmail.com>
2024-11-13 10:25:14 -0400

    Update Aula8.py

--- Aula8.py
+++ Aula8.py
@@ -1,3 +1,12 @@
 import streamlit as st
 
-st.write("Teste")
+st.write("Este é meu primeiro Deploy")
+
+
+st.title("Este é o título do app")
+st.header("Este é o subtítulo")
+st.subheader("Este é o terceiro subtítulo")
+st.markdown("Este é texto")
+st.caption("Esta é a a legenda")
+st.code("x=2021")
+st.latex(r''' a+a r^1+a r^2+a r^3 ''')
