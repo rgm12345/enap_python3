7d17fe75bd54afeb3017529bca44ca8d0a0bb012
rgm12345 <rebeca.guerreiromachado@gmail.com>
2024-11-13 10:40:36 -0400

    Update Aula8.py

--- Aula8.py
+++ Aula8.py
@@ -11,20 +11,29 @@ st.markdown("Vamos fazer uma tentativa...")
 #st.code("x=2021")
 #st.latex(r''' a+a r^1+a r^2+a r^3 ''')
 
+# criando elementos gráficos
+'''Informar como colher os dados através de variáveis'''
+x = st.checkbox('Sim')
+st.write(x)
 
+# fazer o mesmo com alguns dos comandos abaixo
+st.title(x)
 st.button('Clique')
-st.radio('Selecione seu gênero',['Masculino','Feminino'])
-st.multiselect('Escolha um departamento',['SOF', 'ENAP', 'UFs'])
-st.select_slider('Selecione uma resposta', ['Ruim', 'Bom', 'Excelente'])
-st.slider('Selecione um número', 0,50)
-st.number_input('Selecione um número', 0,10)
-st.text_input('Endereço de e-mail')
-st.date_input('Data de viagem')
-st.time_input('Tempo de escola')
+st.radio('Selecione seu gênero',['Masculino','Feminino'])
+st.selectbox('Selecione seu gênero',['Masculino','Feminino'])
+st.multiselect('Escolha um departamento',['DCS', 'DE', 'DIR'])
+st.select_slider('Selecione uma resposta', ['Ruim', 'Bom', 'Excelente'])
+st.slider('Selecione um número', 0,50)
+st.number_input('Selecione um número', 0,10)
+st.text_input('Endereço de e-mail')
+st.date_input('Data de viagem')
+st.time_input('Tempo de escola')
 st.text_area('Descrição')
-st.file_uploader('Atualize uma foto')
-st.color_picker('Escolha sua cor favorita')
+st.file_uploader('Atualize uma foto')
+st.color_picker('Escolha sua cor favorita')
 
-
-
-
+# mensagens de status
+st.success("Você conseguiu!")
+st.error("Erro!")
+st.warning("Advertência")
+st.info("Esta é uma informação")
